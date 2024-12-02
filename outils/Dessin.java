61b4ebcc1dbf4c481a26b4d4adc819cdc98f90c6
Théo DISY <122993572+Souyyy@users.noreply.github.com>
2024-12-02 22:41:12 +0100

    Update Dessin.java

--- outils/Dessin.java
+++ outils/Dessin.java
@@ -5,19 +5,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Dessin {
-    private List<Point> points; // Liste des points dessinés
-    private Color couleur; // Couleur du dessin
-    private static final int RAYON_TOLERANCE = 5; // Rayon de tolérance pour détecter si un point est dans le dessin
+    // Liste des points dessinés
+    private List<Point> points; 
+    // Couleur du dessin
+    private Color couleur; 
+    // Rayon de tolérance pour détecter si un point est dans le dessin
+    private static final int RAYON_TOLERANCE = 5; 
 
+    // Constructeur pour init la couleur du dessin
     public Dessin(Color couleur) {
         this.couleur = couleur;
         this.points = new ArrayList<>();
     }
 
+    // Ajout dun point au dessin
     public void ajouterPoint(Point point) {
         points.add(point);
     }
 
+    // Dessine le dessin sur le composant graphique
     public void dessiner(Graphics2D g2d) {
         g2d.setColor(couleur);
         for (int i = 0; i < points.size() - 1; i++) {
