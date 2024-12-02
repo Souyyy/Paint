ce04815c89e26783ce7decc1e2ac2d19c539c43d
Théo DISY <122993572+Souyyy@users.noreply.github.com>
2024-12-02 22:39:14 +0100

    Update Forme.java

--- formes/Forme.java
+++ formes/Forme.java
@@ -2,35 +2,41 @@ package formes;
 
 import java.awt.*;
 
+// Classe abstraite Forme, servant de base pour toutes les formes
 public abstract class Forme {
     protected Color couleur;
-    public int x; // Position initiale
+    // Position initiale
+    public int x; 
     public int y;
-    protected int largeur, hauteur; // Dimensions
-    protected static final int TAILLE_POIGNET = 8; // Taille des poignées de redimensionnement
+    protected int largeur, hauteur; 
 
+    // Taille des poignes de redimensionnement
+    protected static final int TAILLE_POIGNET = 8; 
+
+    // Constructeur pour init la couleur et la position
     public Forme(Color couleur, int x, int y) {
         this.couleur = couleur;
         this.x = x;
         this.y = y;
     }
 
+    // Méthode abstraite
     public abstract void dessiner(Graphics2D g);
-
     public abstract boolean contientPoint(int px, int py);
 
-    // Méthodes de déplacement et de redimensionnement
+    // Methode de déplacement 
     public void deplacer(int dx, int dy) {
         this.x += dx;
         this.y += dy;
     }
 
+    // Methode pour redimensionner la forme
     public void redimensionner(int largeur, int hauteur) {
         this.largeur = largeur;
         this.hauteur = hauteur;
     }
 
-    // Dessine les poignées de redimensionnement (petits carrés autour de la forme)
+    // Dessine les poignees de redimensionnement (petits carrés autour de la forme)
     public void dessinerPoignees(Graphics2D g) {
         g.setColor(Color.RED);
         g.fillRect(x - TAILLE_POIGNET / 2, y - TAILLE_POIGNET / 2, TAILLE_POIGNET, TAILLE_POIGNET); // Coin supérieur gauche
