4b7e5aee0228e8d3d089845d892139456f3fcb68
Théo DISY <122993572+Souyyy@users.noreply.github.com>
2024-12-02 22:42:56 +0100

    Update Gomme.java

--- outils/Gomme.java
+++ outils/Gomme.java
@@ -7,6 +7,7 @@ public class Gomme {
     private boolean modeGomme; // Indique si le mode gomme est activé
     private ZoneDessin zoneDessin; // Référence à la zone de dessin pour effectuer l'effacement
 
+    // Constructeur qui init la gomme
     public Gomme(ZoneDessin zoneDessin) {
         this.zoneDessin = zoneDessin;
         this.modeGomme = false; // Le mode gomme est désactivé par défaut
@@ -17,12 +18,12 @@ public class Gomme {
         modeGomme = mode;
     }
 
-    
+    // Retourne l'état actuel du mode gomme
     public boolean isModeGomme() {
         return modeGomme;
     }
 
-    
+    //effacement avec la gomme
     public void gestionGomme(MouseEvent e) {
         if (modeGomme) {
             int x = e.getX();
